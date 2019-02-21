e8403bbee57c83cf130c4c119ca63970ab2c452f
ArkadiuszGarzel <ejkej87@gmail.com>
2019-02-21 19:45:24 +0100

    Add js files

--- components/App.js
+++ components/App.js
@@ -1,51 +1,63 @@
 App = React.createClass({
-   render: function () {
-      var styles = {
-         margin: '0 auto',
-         textAlign: 'center',
-         width: '90%'
-      },
+	getInitialState() {
+		return {
+			loading: false,
+			searchingText: '',
+			gif: {}
+		};
+	};
 
-      return (
-         <div style={styles}>
-            <h1> Wyszukiwarka GIFow!</h1> 
-              <p>Znajdź gifa na < a href = 'http://giphy.com' > giphy < /a>. Naciskaj enter,
-                  aby pobrać kolejne gify.</p>
-              <Search onSearch = {this.handleSearch}/> 
-              <Gif loading = {this.state.loading} url = {this.state.gif.url} sourceUrl = {this.state.gif.sourceUrl}/>
-         </div>
-      ),
-   },
-   
-   handleSearch: function (searchingText) {
-      this.setState({
-         loading: true
-      });
-      this.getGif(searchingText, function (gif) {
-         this.setState({
-            loading: false,
-            gif: gif,
-            searchingText: searchingText
-         });
-      }.bind(this));
-   },
+	handleSearch: function(searchingText) {
+		this.setState({
+			loading: true
+		});
+		this.getGif(searchingText, function(gif) {
+			this.setState({
+				loading: false,
+				gif: gif,
+				searchingText: searchingText
+			})
+		}.bind(this));
+	};
 
-   getGif: function (searchingText, callback) {
-      var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText; // 2.
-      var xhr = new XMLHttpRequest();.
-      xhr.open('GET', url);
-      xhr.onload = function () {
-         if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText).data;
-            var gif = {
-               url: data.fixed_width_downsampled_url,
-               sourceUrl: data.url
-            };
-            callback(gif);
-         };
-         xhr.send();
-      },
-   }
-      
+	getGif: function(searchingText, callback) {
+		var GIPHY_API_URL = 'https://api.giphy.com';
+		var GIPHY_PUB_KEY = 'Zo06Anmw3BLW6loJZvnm97LJHKeUGS2h';
+		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.onload = function() {
+			if (xhr.status === 200) {
+				var data = JSON.parse(xhr.responseText).data;
+				var gif = {
+					url: data.fixed_width_downsampled_url,
+					sourceUrl: data.url
+				}
+				callback(gif);
+			}
+		}
+		xhr.send();
+	};
+
+	render: function() {
+
+		var style = {
+			margin: '0 auto',
+			textAlign: 'center',
+			width: '90%'
+		};
+
+		return (
+			<div style={style}>
+				<h1>Gifs search engine!</h1> 
+				<p>Find gifs on <a href='http://giphy.com'> giphy </a>. Press enter to get more gifs.</p>
+				<Search onSearch={this.handleSearch}/>
+				<Gif
+	                loading={this.state.loading}
+	                url={this.state.gif.url}
+	                sourceUrl={this.state.gif.sourceUrl}
+            	/>
+			</div>
+		)
+	}
 });
-
--- components/Gif.js
+++ components/Gif.js
@@ -1,8 +1,8 @@
-var GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif'
+var GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif';
 var styles = {
   minHeight: '310px',
   margin: '0.5em'
-}
+};
 
 Gif = React.createClass({
   getUrl: function () {
